cf4d655759bb31286bfa20d6df4041f93b2013e4
Francisco AC <bepitic@gmail.com>
2020-02-05 17:42:46 +0000

    add script to hide related vid and comments sections from a video.

---
+++ Hide_RelatedVids.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Hide youtube related
+// @namespace    bepitic
+// @version      0.1
+// @description  Deactivate recomendations from youtube
+// @author       Bepitic
+// @match        https://www.youtube.com/**
+// @require https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
+// @require https://raw.githubusercontent.com/moderntribe/tampermonkey-scripts/master/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+
+
+$( document ).ready(function() {
+
+    function hide_r(){
+                document.getElementById("related").style.display = "none";
+    }
+    waitForKeyElements ("#related", hide_r);
+
+    function hide_s(){
+                document.getElementById("sections").style.display = "none";
+    }
+    waitForKeyElements ("#sections", hide_s);
+});
